f204b06e0b6db6413fe6ec7bbefddf12232e7434
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-08-01 13:19:53 +0530

    implement QorR code

--- PowerOf2Program1/PowerOf2Program1/Program.cs
+++ PowerOf2Program1/PowerOf2Program1/Program.cs
@@ -1,42 +1,26 @@
 ﻿using System;
 
-namespace PowerOf2Program1
+namespace QuotientOrReminder
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            try
-            {
-                int no;
-                int remainder;
-                Console.WriteLine("Enter a no to check");
-                no = Convert.ToInt32(Console.ReadLine());
 
-                if (no == 1)
-                    Console.Write("It is power of 2.");
-                else
-                {
-                    while (no > 1)
-                    {
-                        remainder = no % 2;
-                        if (remainder != 0)
-                            break;
-                        no /= 2;
-                    }
-                    if (no == 1)
-                        Console.WriteLine("It is a power of 2");
-                    else
-                        Console.WriteLine("It is not a power of 2");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            int dividend, divisor;
+            int remainder = 0;
+            Console.Write("Enter the dividend :");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the divisor :");
+            divisor = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+
+            int quotient = dividend / divisor;
+            remainder = dividend % divisor;
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+
         }
     }
 }
